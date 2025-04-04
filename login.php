f91482f72f15e44b33379e813a7dd04294a75b27
kalppatel5530 <kalppatel.aorc@gmail.com>
2025-04-04 18:29:40 +0530

    Initial commit

--- login.php
+++ login.php
@@ -7,6 +7,43 @@
     <title>Login Page</title>
 </head>
 <body>
-  
+  <div class="center">
+
+    <h1>Login</h1>
+    <form action="" method="POST">
+    <div class="form">
+<input type="text" name="username" class="textfield" placeholder="Username"><br><br>
+<input type="password" name="password" class="textfield" placeholder="Password">
+
+<div class="forgetpass"><a href="#" class="link" onclick="message()">Forget Password?</a></div>
+
+<input type="submit" class="btn" name="login" value="Login">
+
+<div class="signup">New Member ? <a href="form.php" class="link">SignUp Here</a></div>
+</div>
+  </div>
+</form>
+  <script>
+    function message(){
+        alert("Pls Password yad karle bhai");
+    }
+  </script>
 </body>
 </html>
+
+<?php
+include('connection.php');
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $pwd = $_POST['password'];
+
+    $query="select * from form where email='$username' && password='$pwd'";
+
+    $data = mysqli_query($conn , $query);
+
+    $total = mysqli_num_rows($data);
+    // echo $total;
+}
+?>
